5f0cb83a038bb9bee7552a1bb1df3fb643f13b74
WUXIN <myeagleandsnake@gmail.com>
2024-03-14 10:09:34 +0800

    feat:

--- ServiceA/src/main/java/org/wx/controller/AControllerOne.java
+++ ServiceA/src/main/java/org/wx/controller/AControllerOne.java
@@ -15,20 +15,21 @@ public class AControllerOne {
 
     @GetMapping("/a/hello-world")
     @SentinelResource(value = "hello", blockHandler = "exceptionHandler", fallback = "helloFallback")
-    public String helloImA(){
+    public String helloImA() throws InterruptedException {
+        Thread.sleep(3000);
         return "Hello I am A";
     }
 
 
     // Fallback 函数，函数签名与原函数一致或加一个 Throwable 类型的参数.
-    public String helloFallback(long s) {
-        return String.format("Halooooo %d", s);
+    public String helloFallback() {
+        return String.format("Halooooo %d");
     }
 
     // Block 异常处理函数，参数最后多一个 BlockException，其余与原函数一致.
-    public String exceptionHandler(long s, BlockException ex) {
+    public String exceptionHandler(BlockException ex) {
         // Do some log here.
         ex.printStackTrace();
-        return "Oops, error occurred at " + s;
+        return "Oops, error occurred at " ;
     }
 }
