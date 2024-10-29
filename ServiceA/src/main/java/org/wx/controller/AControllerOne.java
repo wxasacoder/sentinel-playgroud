29d3a93ae4ac7702dd08e207f58d294138e10d73
wuxin <myeagleandsnake@gaml.com>
2024-10-30 00:06:54 +0800

    feat: 探索1.0

--- ServiceA/src/main/java/org/wx/controller/AControllerOne.java
+++ ServiceA/src/main/java/org/wx/controller/AControllerOne.java
@@ -4,6 +4,7 @@ import com.alibaba.csp.sentinel.annotation.SentinelResource;
 import com.alibaba.csp.sentinel.slots.block.BlockException;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.wx.config.MyUrlBlockHandler;
 
@@ -27,9 +28,9 @@ public class AControllerOne {
     }
 
     // Block 异常处理函数，参数最后多一个 BlockException，其余与原函数一致.
-    public String exceptionHandler(long s, BlockException ex) {
+    public static String exceptionHandler(BlockException ex) {
         // Do some log here.
         ex.printStackTrace();
-        return "Oops, error occurred at " + s;
+        return "Oops, error occurred at " ;
     }
 }
