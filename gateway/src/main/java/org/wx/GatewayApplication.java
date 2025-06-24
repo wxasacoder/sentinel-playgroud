944996dfccf17600f34012387d992fa5222b55c8
WUXIN <myeagleandsnake@gmail.com>
2025-06-24 15:55:41 +0800

    feat

---
+++ gateway/src/main/java/org/wx/GatewayApplication.java
@@ -0,0 +1,17 @@
+package org.wx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author wuxin
+ * @date 2025/06/24 15:30:22
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class,args);
+    }
+}
