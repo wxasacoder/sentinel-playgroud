34eb09e850090b4ad3344d7f078c7b21b875b9b2
wuxin <myeagleandsnake@gaml.com>
2025-06-26 00:02:55 +0800

    feat:本地文件规则存储

--- ServiceC/src/main/java/org/wx/controller/AControllerOne.java
+++ ServiceC/src/main/java/org/wx/controller/AControllerOne.java
@@ -16,16 +16,4 @@ public class AControllerOne {
         return "Hello I am b";
     }
 
-
-    // Fallback 函数，函数签名与原函数一致或加一个 Throwable 类型的参数.
-    public String helloFallback(long s) {
-        return String.format("Halooooo %d", s);
-    }
-
-    // Block 异常处理函数，参数最后多一个 BlockException，其余与原函数一致.
-//    public static String exceptionHandler(BlockException ex) {
-//         Do some log here.
-//        ex.printStackTrace();
-//        return "Oops, error occurred at " ;
-//    }
 }
